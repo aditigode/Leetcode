2272e7b2ae12246b35951f4af29b3c24be7500f5
Aditi Gode <89469875+aditigode@users.noreply.github.com>
2023-10-16 13:34:00 -0400

    Time: 2732 ms (27.1%), Space: 437.6 MB (38.11%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.py
+++ 2808-painting-the-walls/2808-painting-the-walls.py
@@ -3,31 +3,27 @@ class Solution:
 
         walls = len(cost)
         index = len(cost)
-        dp = [[-1 for _ in range(index)]for _ in range(walls+1)]
-        @cache
+        #dp = [[-1 for _ in range(index)]for _ in range(walls+1)]
+        dp = {}
+        
         def dfs(i, remainWalls):
             if remainWalls <= 0:
                 return 0
             if i == index:
                 return math.inf
 
-            if dp[remainWalls][i] > -1:
-                return dp[remainWalls][i]
+            if (remainWalls,i) in dp:
+                return dp[(remainWalls,i)]
 
            
 
             paint = cost[i] + dfs(i+1, remainWalls - time[i] - 1)
             skip = dfs(i+1, remainWalls)
-            dp[remainWalls][i] = min(paint, skip)
-            return dp[remainWalls][i]
+            dp[(remainWalls,i)] = min(paint, skip)
+            return dp[(remainWalls,i)]
 
-        dfs(0,len(cost))
-        min_cost = math.inf
-        #print(dp)
-        for i in range(index):
-            min_cost = min(dp[walls][i], min_cost)
-
-        return min_cost
+        return dfs(0,len(cost))
+       
 
 
         
