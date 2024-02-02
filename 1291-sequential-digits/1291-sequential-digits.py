8127418557bdcd11037bf42f3027b5e1e1132d32
Aditi Gode <89469875+aditigode@users.noreply.github.com>
2024-02-02 13:09:29 -0600

    Time: 39 ms (40.37%), Space: 16.5 MB (63.35%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,8 +1,11 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
         window ="123456789"
+        
         result = []
         for i in range(len(str(low)),len(str(high))+1):
+            # 10-i to is adjust for the last window. Eg: if len(low)=3 last window should be 7:7+3= 7:10: 789
+            # 10-i will prevent index out of range error
             for j in range(10-i):
                 num = int(window[j:j+i])
                 if low <= num <= high:
